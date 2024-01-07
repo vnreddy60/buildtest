9bbabc1e00c07cd889048dd82214b244162a7bcd
Kalluri Vinay Reddy <vnreddy60@gmail.com>
2024-01-06 19:05:25 -0600

    Initial commit

---
+++ src/main/java/com/example/buildtest/BuildtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buildtest/BuildtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
